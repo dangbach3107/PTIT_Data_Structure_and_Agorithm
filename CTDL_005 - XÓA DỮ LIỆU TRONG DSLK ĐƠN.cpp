20256fa989075fc4bf4c6465012f3f548605a93b
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 05:42:56 +0700

    Create CTDL_005 - XÓA DỮ LIỆU TRONG DSLK ĐƠN.cpp

---
+++ CTDL_005 - XÓA DỮ LIỆU TRONG DSLK ĐƠN.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen ("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    int a[n];
+    for(auto &x : a) cin >> x;
+    int m; cin >> m;
+    for(int x : a) if(x != m) cout << x << " ";
+}
+
+int main(){
+    //read
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
