f73d4d4784615e94607cd26ad648e53e45fe7512
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-27 23:04:48 +0700

    Create CHELLO - Hello World.cpp

---
+++ CHELLO - Hello World.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen ("output.txt", "w", stdout);
+using namespace std;
+
+int main(){
+    cout << "Hello PTIT.";
+}
