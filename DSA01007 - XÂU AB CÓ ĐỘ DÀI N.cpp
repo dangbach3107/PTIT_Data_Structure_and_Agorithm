8469e8d6a37d04f0c4de64f2cc19bba679e8787b
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 05:51:52 +0700

    Create DSA01007 - XÂU AB CÓ ĐỘ DÀI N.cpp

---
+++ DSA01007 - XÂU AB CÓ ĐỘ DÀI N.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define ll long long
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    string s = "";
+    for(int  i = 1; i <=n; i++){
+        s += "A";
+    }
+    while(1){
+        cout << s << " ";
+        int pos = n - 1;
+        while(s[pos] == 'B' && pos > -1){
+            s[pos] = 'A';
+            pos--;
+        }
+        if(pos == -1) return;
+        s[pos] = 'B';
+    }
+    
+}
+
+int main() {
+   // read
+    int t = 1; 
+    cin >> t;
+    while(t--){
+        solve();cout << endl;
+    }
+}
