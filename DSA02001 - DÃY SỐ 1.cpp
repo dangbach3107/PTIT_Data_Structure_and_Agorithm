ff58bf7c9decba1c3cbe4b613ba0d83935b1bce3
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 05:54:50 +0700

    Create DSA02001 - DÃY SỐ 1.cpp

---
+++ DSA02001 - DÃY SỐ 1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define ll long long
+using namespace std;
+
+int n;
+vector<int> a, b;
+
+void in(){
+    cout << "[";
+    for(int i = 0; i < a.size(); i++){
+        cout << a[i];
+        if(i != a.size() - 1) cout << " ";
+    }
+    cout <<"]\n";
+}
+
+void Try(int i){
+    in();
+    b.resize(i - 1);
+    for(int j = 0; j < i - 1; j++){
+        b[j] = a[j] + a[j + 1];
+    }
+    if(i == 1) return;
+    a = b;
+    a.resize(i - 1);
+    b.clear();
+    Try(i - 1);
+    
+}
+
+void solve(){
+    cin >> n;
+    a.resize(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    Try(n);
+}
+
+int main(){
+    //read
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
