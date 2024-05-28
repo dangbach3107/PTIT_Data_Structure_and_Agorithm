1b01ed5824030516d07009777620335e9e01495c
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:05:30 +0700

    Create DSA07008 - BIẾN ĐỔI TRUNG TỐ - HẬU TỐ.cpp

---
+++ DSA07008 - BIẾN ĐỔI TRUNG TỐ - HẬU TỐ.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int priority(char c){
+	if(c == '^') return 3;
+	if(c == '*' || c == '/') return 2;
+	if(c == '+' || c == '-') return 1;
+	return 0;
+}
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		string s;cin >> s;
+		stack <char> st;
+		for(int i = 0 ; i < s.size(); i++){
+			if(s[i] == '(') st.push(s[i]);
+			else if(s[i] == ')'){
+				while(st.top() != '('){
+					cout << st.top(); st.pop();
+				}
+				st.pop();
+			}
+			else if(s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^'){
+				while(!st.empty() && priority(s[i]) <= priority(st.top())){
+					cout << st.top(); st.pop();
+				}
+				st.push(s[i]);
+			}
+			else cout << s[i];
+		}
+		while(!st.empty()) {
+			if(st.top() != '(')cout << st.top(); st.pop();
+		}
+		cout << endl;
+	}
+}
