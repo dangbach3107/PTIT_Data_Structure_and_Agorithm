ca56227b340b93f6a11690bac696ec8108231017
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 09:13:57 +0700

    Sinh kế tiếp

--- DSA01026 - PHÁT LỘC.cpp
+++ DSA01026 - PHÁT LỘC.cpp
@@ -2,7 +2,7 @@
 #define f0(i, a) for(int i = 0; i < a; i++)
 #define f1(i, a) for(int i = 1; i <= a; i++)
 #define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
-#define ll long long
+#define ll long long 
 using namespace std;
 
 bool check(string s){
