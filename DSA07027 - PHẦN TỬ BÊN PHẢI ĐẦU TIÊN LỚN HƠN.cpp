93d105a3dbc206b5306cd3726b209d04505c94d1
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:00:35 +0700

    Create DSA07027 - PHẦN TỬ BÊN PHẢI ĐẦU TIÊN LỚN HƠN.cpp

---
+++ DSA07027 - PHẦN TỬ BÊN PHẢI ĐẦU TIÊN LỚN HƠN.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	int n; cin >> n;
+	int a[n], r[n];
+	for(auto &x : a) cin >> x;
+	stack<int> st;
+
+	for(int i = n - 1; i >= 0; i--){
+		while(!st.empty() && st.top() <= a[i]){
+			st.pop();
+		}
+		if(st.empty()){
+			r[i] = -1;
+
+		}
+		else r[i] = st.top();
+		st.push(a[i]);
+	}
+	for(auto x : r) cout << x << ' ';
+	
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		solve();
+		cout << endl;
+	}
+}
