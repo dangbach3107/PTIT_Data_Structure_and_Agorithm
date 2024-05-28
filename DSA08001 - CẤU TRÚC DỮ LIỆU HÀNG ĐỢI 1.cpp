2f03ce2a8c18df928080272688e76db81c0b0490
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:38:57 +0700

    Create DSA08001 - CẤU TRÚC DỮ LIỆU HÀNG ĐỢI 1.cpp

---
+++ DSA08001 - CẤU TRÚC DỮ LIỆU HÀNG ĐỢI 1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define ll long long
+using namespace std;
+void solve(){
+    int t; cin >> t;
+    queue <int> q;
+    while(t--){
+        int x; cin >> x;
+        if(x == 1){
+            cout << q.size() << endl;
+        }
+        else if(x == 2){
+            if(q.empty()) cout << "YES\n";
+            else cout << "NO\n";
+        }
+        else if(x == 3){
+            int tmp; cin >> tmp;
+            q.push(tmp);
+        }
+        else if(x == 4){
+            if(!q.empty()) q.pop();
+        }
+        else if(x == 5){
+            if(!q.empty()) cout << q.front() << endl;
+            else cout << "-1" << endl;
+        }
+        else {
+            if(!q.empty()) cout << q.back() << endl;
+            else cout << "-1" << endl;
+        }
+    }
+}
+
+int main(){
+    //read
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+        cout << endl;
+    }
+}
