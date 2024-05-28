a46f5e378ca17d577122147b5f271156f1e426c2
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 07:59:15 +0700

    Create DSA06018 - BỔ SUNG PHẦN TỬ.cpp

---
+++ DSA06018 - BỔ SUNG PHẦN TỬ.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    set <int> s;
+    while(n--){
+        int x; cin >> x;
+        s.insert(x);
+    }
+    cout << *s.rbegin() - *s.begin() + 1 - s.size() << endl;
+}
+
+int main() {
+    //read
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
