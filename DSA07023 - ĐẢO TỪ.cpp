aa78aa171c2e8a5a4648c583b12403d379223af9
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 09:47:47 +0700

    Create DSA07023 - ĐẢO TỪ.cpp

---
+++ DSA07023 - ĐẢO TỪ.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	string s; getline(cin , s);
+	stack <string > st;
+	stringstream ss(s);
+	string token;
+	while(ss >> token){
+		st.push(token);
+	}
+	while(!st.empty()){
+		cout << st.top() << ' ';
+		st.pop();
+	}
+}
+
+int main() {
+	int t; cin >> t; cin.ignore();
+	while (t--) {
+		solve();
+		cout << endl;
+	}
+}
