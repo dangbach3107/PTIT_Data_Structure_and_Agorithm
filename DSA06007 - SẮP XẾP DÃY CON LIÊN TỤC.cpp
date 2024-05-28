f5859cd11e72a3a27622daa0000eb34401544535
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 07:56:39 +0700

    Create DSA06007 - SẮP XẾP DÃY CON LIÊN TỤC.cpp

---
+++ DSA06007 - SẮP XẾP DÃY CON LIÊN TỤC.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen ("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    vector <int> a(n), b;
+    for(auto &x : a) cin >> x;
+    b = a;
+    sort(a.begin(), a.end());
+    int l = 0, r = n - 1;
+    while(a[l] == b[l]) l++;
+    while(a[r] == b[r]) r--;
+    cout << l + 1 << " " << r + 1 << endl;
+}
+
+int main(){
+    //read
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
