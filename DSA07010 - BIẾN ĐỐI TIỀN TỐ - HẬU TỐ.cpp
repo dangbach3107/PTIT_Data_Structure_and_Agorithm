714daaf649c7b8fc09f6a6cbbc25d843dda7f474
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 09:50:25 +0700

    Create DSA07010 - BIẾN ĐỐI TIỀN TỐ - HẬU TỐ.cpp

---
+++ DSA07010 - BIẾN ĐỐI TIỀN TỐ - HẬU TỐ.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		string s;cin >> s;
+		stack <string> st;
+		for(int i = s.size() - 1; i >= 0; i--){
+			string tmp = string(1, s[i]);
+			if(tmp == "*" || tmp == "/" || tmp == "+" || tmp == "-"){
+				string x = st.top(); st.pop();
+				string y = st.top(); st.pop();
+				string z = x + y + tmp;
+				st.push(z);
+			}
+			else st.push(tmp);
+		}
+		cout << st.top() << endl;
+	}
+}
