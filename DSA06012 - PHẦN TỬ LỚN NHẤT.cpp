d3e0f61efdf5e626a15b2043be694e2cf31b7e5c
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 08:38:00 +0700

    Sắp xếp - Tìm kiếm

--- DSA06012 - PHẦN TỬ LỚN NHẤT.cpp
+++ DSA06012 - PHẦN TỬ LỚN NHẤT.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define f0(i, a) for(int i = 0; i < a; i++)
 #define f1(i, a) for(int i = 1; i <= a; i++)
-#define read freopen("input.txt", "r", stdin); freopen ("output.txt", "w", stdout);
+#define read freopen("input.txt", "r", stdin); freopen ("output.txt", "w", stdout); 
 using namespace std;
 
 void solve(){
