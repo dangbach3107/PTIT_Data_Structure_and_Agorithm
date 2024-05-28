7dbe56784e955e8efdf16bd41fa9ffc8e5423654
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 13:34:10 +0700

    Create DSA10005 - CHU TRÌNH EULER TRONG ĐỒ THỊ CÓ HƯỚNG.cpp

---
+++ DSA10005 - CHU TRÌNH EULER TRONG ĐỒ THỊ CÓ HƯỚNG.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define ll long long
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector <vector<int>> g(n + 1, vector<int>(n + 1, 0));
+    while(k--){
+        int x, y;
+         cin >> x >> y;
+        g[x][y] = 1;
+    }
+    for(int i = 1; i <= n; i++){
+        int dem1 = 0, dem2 = 0;
+        for(int x = 1; x <= n; x++){
+            if(g[x][i] == 1) dem1++;
+        }
+        for(int x = 1; x <= n; x++){
+            if(g[i][x] == 1) dem2++;
+        }
+        if(dem1 != dem2) {
+            cout << 0; return;
+        }
+    }
+    cout << 1;
+}
+
+int main(){
+   // read
+    int t = 1;
+    cin >> t; 
+    while(t--){
+        solve();
+        cout << endl;
+    }
+}
