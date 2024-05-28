2c01286e92287c0b0a6ee7d8349ccd218b566943
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:18:35 +0700

    Ngăn xếp

--- DSA07010 - BIẾN ĐỐI TIỀN TỐ - HẬU TỐ.cpp
+++ DSA07010 - BIẾN ĐỐI TIỀN TỐ - HẬU TỐ.cpp
@@ -8,7 +8,7 @@ int main(){
 		string s;cin >> s;
 		stack <string> st;
 		for(int i = s.size() - 1; i >= 0; i--){
-			string tmp = string(1, s[i]);
+			string tmp = string(1, s[i]); 
 			if(tmp == "*" || tmp == "/" || tmp == "+" || tmp == "-"){
 				string x = st.top(); st.pop();
 				string y = st.top(); st.pop();
