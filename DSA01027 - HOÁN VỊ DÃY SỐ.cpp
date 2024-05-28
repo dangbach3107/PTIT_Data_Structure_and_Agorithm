1bc24fcc06eb9884c38019a48d0634c3d11812fd
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 09:14:04 +0700

    Sinh kế tiếp

--- DSA01027 - HOÁN VỊ DÃY SỐ.cpp
+++ DSA01027 - HOÁN VỊ DÃY SỐ.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void solve(){
     int n; cin >> n;
     int a[n];
-    for(auto &x : a) cin >> x;
+    for(auto &x : a) cin >> x; 
     sort(a, a + n);
     for(int i = 0 ;  i < n; i++) {
         cout << a[i] << " ";
