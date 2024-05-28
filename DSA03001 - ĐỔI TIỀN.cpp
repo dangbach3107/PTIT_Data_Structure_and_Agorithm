727525671619651f6d12150a409cb8f0e4cea589
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 14:13:09 +0700

    Tham lam

--- DSA03001 - ĐỔI TIỀN.cpp
+++ DSA03001 - ĐỔI TIỀN.cpp
@@ -9,7 +9,7 @@ int main(){
     while(t--){
         int n; cin >> n; 
         int a[10] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000};
-        int dem = 0;
+        int dem = 0; 
         for(int i = 9; i >= 0; i--){
             dem += n / a[i];
             n %= a[i];
