5118ae63290e2ffc919bc52b28116234977c5fd6
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:50:10 +0700

    Hàng đợi

--- DSA08017 - SỐ LỘC PHÁT 1.cpp
+++ DSA08017 - SỐ LỘC PHÁT 1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int priority(char c){
-	if(c == '^') return 3;
+	if(c == '^') return 3; 
 	if(c == '*' || c == '/') return 2;
 	if(c == '+' || c == '-') return 1;
 	return 0;
