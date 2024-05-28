9d7041b7747b522aa14b26cf9afda79468da33d8
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:04:51 +0700

    Create DSA07011 - BIẾN ĐỔI HẬU TỐ - TIỀN TỐ.cpp

---
+++ DSA07011 - BIẾN ĐỔI HẬU TỐ - TIỀN TỐ.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	string s; cin >> s;
+	stack <string> st;
+	for(int i = 0; i< s.size(); i++){
+		if('A' <= s[i] && 'Z' >= s[i]) st.push(string(1, s[i]));
+		else{
+			string t1 = st.top(); st.pop();
+			string t2 = st.top(); st.pop();
+			st.push(string(1, s[i]) + t2 + t1 );
+		}
+	}
+	while(st.size()){
+		cout << st.top();
+		st.pop();
+
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		solve();
+		cout << endl;
+	}
+}
