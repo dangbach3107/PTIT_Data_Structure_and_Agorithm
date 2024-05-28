fe1b705849ec84eb290c15d4cc866960f49e45f3
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:03:45 +0700

    Create DSA07016 - BIỂU THỨC TĂNG GIẢM.cpp

---
+++ DSA07016 - BIỂU THỨC TĂNG GIẢM.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	string s; cin >> s;
+    stack <int> st;
+    int dem = 1;
+    st.push(dem);
+
+    for(auto x : s){
+        if(x == 'D'){
+            st.push(++dem);
+        }
+        else{
+            while(!st.empty()){
+                cout << st.top(); st.pop();
+            }
+            st.push(++dem);
+        }
+    }
+    while(!st.empty()){
+        cout << st.top(); st.pop();
+    }
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		solve();
+		cout << endl;
+	}
+}
