9cecb6af8c7c54d5d4b0dfcb808a5261f3e7e9b5
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 16:05:34 +0700

    Quy hoạch động

---
+++ DSA05005 - SỐ BƯỚC ÍT NHẤT.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define ll long long
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    int a[n + 1];
+    f1(i, n) cin >> a[i];
+    vector <int> dp(n + 1, 1);
+    for(int i = 2; i <= n; i++){
+        for(int j = 1; j < i; j++){
+            if(a[j] <= a[i]) dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+    cout << n - *max_element(dp.begin(), end(dp));
+}
+
+int main(){
+    //read
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+        cout << endl;
+    }
+}
