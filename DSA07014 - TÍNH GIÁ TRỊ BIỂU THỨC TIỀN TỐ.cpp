1d3e9b01e1a16b412c236bee83ddea68e77cf6de
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 09:48:59 +0700

    Create DSA07014 - TÍNH GIÁ TRỊ BIỂU THỨC TIỀN TỐ.cpp

---
+++ DSA07014 - TÍNH GIÁ TRỊ BIỂU THỨC TIỀN TỐ.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		string s;cin >> s;
+		stack <string> st;
+		for(int i = s.size() - 1; i >= 0; i--){
+			string tmp = string(1, s[i]);
+			if(tmp == "*" || tmp == "/" || tmp == "+" || tmp == "-"){
+				string x = st.top(); st.pop();
+				string y = st.top(); st.pop();
+				int res;
+                if(tmp == "*"){
+                    res = stoi(x) * stoi(y);
+                }
+                else if(tmp == "/"){
+                    res = stoi(x) / stoi(y);
+                }
+                else if(tmp == "-"){
+                    res = stoi(x) - stoi(y);
+                }
+                else if(tmp == "+"){
+                    res = stoi(x) + stoi(y);
+                }
+                st.push(to_string(res));
+			}
+			else st.push(tmp);
+		}
+		cout << st.top() << endl;
+	}
+}
