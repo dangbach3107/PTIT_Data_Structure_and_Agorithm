0d511abf39ddc578371d1264825ce02b992dd4e7
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:45:41 +0700

    Create DSA08017 - SỐ LỘC PHÁT 1.cpp

---
+++ DSA08017 - SỐ LỘC PHÁT 1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int priority(char c){
+	if(c == '^') return 3;
+	if(c == '*' || c == '/') return 2;
+	if(c == '+' || c == '-') return 1;
+	return 0;
+}
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		queue <string> q;
+		int n; cin >> n;
+		vector <string> res;
+		q.push("6");
+		q.push("8");
+		while(!q.empty()){
+			string tmp = q.front();
+			q.pop();
+			res.push_back(tmp);
+			if(tmp.size() < n){
+				q.push(tmp + "6");
+				q.push(tmp + "8");
+			}
+		}
+		reverse(res.begin(), res.end());
+		for(auto x : res) cout << x << ' ';
+		cout << endl;
+	}
+}
