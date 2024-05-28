464d914917b90e9796e7200a016abce6a25c1a1f
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 08:19:37 +0700

    Create DSA06034 - CẶP SỐ CÓ TỔNG BẰNG K.cpp

---
+++ DSA06034 - CẶP SỐ CÓ TỔNG BẰNG K.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n; cin >> n;int k ;cin >> k;
+    int a[n];
+    for(auto &x : a) cin >> x ;
+    sort(a, a + n);
+    long long cnt = 0;
+    for(int i = 0; i < n; i++){
+        cnt += upper_bound(a + i + 1, a + n, k - a[i]) - lower_bound(a + i + 1, a + n, k - a[i]);
+    }
+    cout << cnt << endl;
+}
+
+int main() {
+    
+    int t = 1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
