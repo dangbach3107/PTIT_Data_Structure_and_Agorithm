0d28c24b805d9e80ced08e396f036b880f178735
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 14:21:50 +0700

    Tham lam

--- DSA03008 - SẮP XẾP CÔNG VIỆC 1.cpp
+++ DSA03008 - SẮP XẾP CÔNG VIỆC 1.cpp
@@ -8,7 +8,7 @@ bool cmp(pair<int, int> a, pair<int, int> b) {
     if (a.second == b.second)
         return a.first < b.first;
     return a.second < b.second;
-}
+} 
 int main(){
     int t; cin >> t;
     while(t--){
