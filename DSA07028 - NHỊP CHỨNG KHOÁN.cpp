57747ecd492baaf802efee6fcf91033c90a06325
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:01:44 +0700

    Create DSA07028 - NHỊP CHỨNG KHOÁN.cpp

---
+++ DSA07028 - NHỊP CHỨNG KHOÁN.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	int n; cin >> n;
+	int a[n + 1];
+	for(int i = 1; i <= n; i++) cin >> a[i];
+	stack<int> st;
+	for(int i = 1; i <= n; i++){
+		while(!st.empty() && a[st.top()] <= a[i]){
+			st.pop();
+		}
+		if(st.empty()){
+			cout << i << ' '; 
+		}
+		else {
+			cout << i - st.top() << ' ';
+		}
+		st.push(i);
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		solve();
+		cout << endl;
+	}
+}
