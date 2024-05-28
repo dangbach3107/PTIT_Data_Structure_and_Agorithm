9fb3585e13306ee0ec5722a02003ad844a9567cb
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 08:04:18 +0700

    Create DSA06010 - SẮP XẾP CHỮ SỐ.cpp

---
+++ DSA06010 - SẮP XẾP CHỮ SỐ.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen ("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    set <int> s;
+    while(n--){
+    	int x; cin >> x;
+    	while(x){
+    		s.insert(x % 10);
+    		x /= 10;
+		}
+	}
+	for(auto x : s) cout << x << " ";
+	cout << endl;
+}
+
+int main(){
+    //read
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
