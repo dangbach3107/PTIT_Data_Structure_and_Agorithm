ac9e799d45089786f6b735eafa995135332fd170
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:41:27 +0700

    Create DSA08010 - BIẾN ĐỔI SỐ TỰ NHIÊN.cpp

---
+++ DSA08010 - BIẾN ĐỔI SỐ TỰ NHIÊN.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+void solve(){
+	int n; cin >> n;
+    queue <pair<int, int>> q;
+    q.push({n, 0});
+	map <int, int> m;
+	m[n] = 1;
+
+    while(1){
+        auto x = q.front();
+        q.pop();
+        if(x.first == 1){
+            cout << x.second;
+            break;
+        } 
+        if(m[x.first - 1] == 0){
+			 q.push({x.first - 1, x.second + 1});
+			m[x.first - 1] = 1;
+		}
+		for(int i = 2; i <= sqrt(x.first); i++){
+			if(x.first % i == 0 && m[x.first / i] == 0){
+				q.push({x.first / i, x.second + 1});
+				m[x.first / i] = 1;
+			}
+		}
+    }
+}
+
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		solve();
+		cout << endl;
+	}
+}
