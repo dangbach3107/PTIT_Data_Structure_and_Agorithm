47d278d7188069ed101bdf47f5c9a91906e1dc7a
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 13:43:25 +0700

    Create DSA09001 - CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp

---
+++ DSA09001 - CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define f0(i, a) for(int i = 0; i < a; i++)
+#define f1(i, a) for(int i = 1; i <= a; i++)
+#define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define ll long long
+using namespace std;
+
+void solve(){
+    int n, k; cin >> n >> k;
+    vector <vector<int> > g(n + 1, vector<int> (n + 1, 0));
+    while(k--){
+        int x, y; cin >> x>> y;
+        g[x][y] = g[y][x] = 1;
+    }
+    for(int i = 1; i <= n; i++){
+        cout << i << ": ";
+        for(int j = 1; j <= n; j++)
+            if(g[i][j]) cout << j <<  " ";
+        cout << endl;
+    }
+}
+
+int main(){
+    //read
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+        cout << endl;
+    }
+}
