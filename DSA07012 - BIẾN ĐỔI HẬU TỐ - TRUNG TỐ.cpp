45a11351020e49b16dae076d0dfc346b8233b437
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 10:04:16 +0700

    Create DSA07012 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cpp

---
+++ DSA07012 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	string s; cin >> s;
+	stack <string> st;
+	for(int i = 0; i< s.size(); i++){
+		if('A' <= s[i] && 'Z' >= s[i]) st.push(string(1, s[i]));
+		else{
+			string t1 = st.top(); st.pop();
+			string t2 = st.top(); st.pop();
+			st.push("(" + t2 +string(1, s[i]) + t1 + ")");
+		}
+	}
+	while(st.size()){
+		cout << st.top(); st.pop();
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		solve();
+		cout << endl;
+	}
+}
