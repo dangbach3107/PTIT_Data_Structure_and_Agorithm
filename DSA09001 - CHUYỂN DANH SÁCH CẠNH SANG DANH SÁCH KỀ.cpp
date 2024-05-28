6964e7990d60207077bb3354751dd530daccc095
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 13:50:55 +0700

    Đồ thị

--- DSA09001 - CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp
+++ DSA09001 - CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define f0(i, a) for(int i = 0; i < a; i++)
+#define f0(i, a) for(int i = 0; i < a; i++) 
 #define f1(i, a) for(int i = 1; i <= a; i++)
 #define read freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
 #define ll long long
