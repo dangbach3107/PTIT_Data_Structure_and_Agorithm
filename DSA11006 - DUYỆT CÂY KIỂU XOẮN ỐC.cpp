6b1e1360b81648e32a01eea4dd3aa21966ec3042
dangbach3107 <144685407+dangbach3107@users.noreply.github.com>
2024-05-28 15:46:36 +0700

    Cây nhị phân

---
+++ DSA11006 - DUYỆT CÂY KIỂU XOẮN ỐC.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+struct node{
+	int data;
+	node *left;
+	node *right;
+	//constructor
+	// node (int x){
+	// 	data = x;
+	// 	left = right = NULL;
+	// }
+};
+
+node *makeNode(int x){
+	node *newNode = new node();
+	newNode -> data = x;
+	newNode -> left = newNode -> right = NULL;
+	return newNode;
+}
+// sau khi tim duoc node cha
+void makeRoot(node *root, int u, int v, char c){
+	if(c == 'L') root->left = makeNode(v);
+	else root->right = makeNode(v);
+}
+
+void insertNode(node *root, int u, int v, char c){
+	if(root == NULL) return;
+	if(root->data == u){
+		makeRoot(root, u, v, c);
+	}
+	else{
+		insertNode(root->left, u, v, c);
+		insertNode(root->right, u, v, c);
+	}
+}
+
+void levelOrder(node *root){
+	queue <node*> q;
+	q.push(root);
+	while(!q.empty()){
+		node *tmp = q.front(); q.pop();
+		cout << tmp->data <<" ";
+		if(tmp->left != NULL){
+			q.push(tmp->left);
+		}
+		if(tmp->right != NULL){
+			q.push(tmp->right);
+		}
+	}
+	cout << endl;
+}
+
+void spiralOrder(node *root){
+	stack <node*> s1, s2;
+	s1.push(root);
+	while(!s1.empty() || !s2.empty()){
+		while(!s1.empty()){
+			node *tmp = s1.top(); s1.pop();
+			cout << tmp->data << ' ';
+			if(tmp->right != NULL) s2.push(tmp->right);
+			if(tmp->left != NULL) s2.push(tmp->left); 
+		}
+		while(!s2.empty()){
+			node *tmp = s2.top(); s2.pop();
+			cout << tmp->data << ' ';
+			if(tmp->left != NULL) s1.push(tmp->left);
+			if(tmp->right != NULL) s1.push(tmp->right); 
+		}
+	}
+	cout << endl;
+}
+
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int n; cin >> n;
+		node *root = NULL;
+		while(n--){
+			int u, v; char c; cin >> u >> v >> c;
+			if(root == NULL){
+				root = makeNode(u);
+				makeRoot(root, u, v, c);
+			}
+			else insertNode(root, u, v, c);
+		}
+		spiralOrder(root);
+	}
+}
